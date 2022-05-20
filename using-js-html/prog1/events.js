602b01d2353722f0ac56433711858bc860aea77f
Guilherme Rodrigues Lopes Viana <guilherme.viana@certsys.com.br>
2022-05-20 19:03:51 -0300

    practicing js on html

---
+++ using-js-html/prog1/events.js
@@ -0,0 +1,17 @@
+function clickMe() {
+    console.log("fui clicado");
+}
+
+function focusMe() {
+    console.log("fui focado");
+}
+function blurMe() {
+    console.log("fui desfocado");
+}
+function changeMe() {
+    console.log("fui alterado");
+}
+
+function overMe() {
+    console.log("Mouse aqui");
+}

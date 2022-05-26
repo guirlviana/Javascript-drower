1546cc51211293629b21fd34bbf7a3733ab7d783
Guilherme Rodrigues Lopes Viana <guilherme.viana@certsys.com.br>
2022-05-25 22:35:21 -0300

    Module as ES6

---
+++ using-js-html/module02/defenses.js
@@ -0,0 +1,9 @@
+export const laserDefenses = ["Defense 01", "Defense 02"]
+export const steelDefense = ["Steels defense"]
+
+// export {
+//     laserDefenses,
+//     steelDefense
+// }
+
+export default ["Basic Defense"]
